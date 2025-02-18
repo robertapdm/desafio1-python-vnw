cc738eb20d6abf2db994b6cc035b418c2b06b7da
robertapdm <robertafarmaco@gmail.com>
2025-02-18 12:46:05 -0300

    creating project solving problems in python

---
+++ main.py
@@ -0,0 +1,23 @@
+# MISSÃO 1
+
+nota = 7
+
+
+if  nota >=6 :
+    print ("Você foi aprovado")
+else:
+    print("Você foi reprovado")
+
+# MISSÃO 2
+
+idade = ("Informe a sua idade")
+
+if idade >= 16:
+    print("Você pode votar")
+else:
+    print("Você não pode votar")
+
+
+# MISSÃO 
+
+
