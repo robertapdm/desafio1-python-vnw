85a49361dd01dda1f44946504b41223018c835c1
robertapdm <robertafarmaco@gmail.com>
2025-02-19 12:53:14 -0300

    solving mission 2 and 3

--- main.py
+++ main.py
@@ -1,23 +1,45 @@
-# MISSÃO 1
+"""# MISSAO 1
 
-nota = 7
+def verificar_aprovacao (nota):
+    if  nota >= 6 :
+        return ("Aprovado")
+    else:
+        return ("Reprovado")
 
+nota = float(input("Insira a nota do aluno: "))
+resultado = verificar_aprovacao (nota)
+print(f"O Aluno está {resultado}.")
 
-if  nota >=6 :
-    print ("Você foi aprovado")
-else:
-    print("Você foi reprovado")
+# MISSAO 2
+ 
+def elegibilidade_votacao (idade):
+    if idade >= 16:
+        return ("pode votar")
+    else:
+        return ("não pode votar")
 
-# MISSÃO 2
+idade = int(input("Insira a sua idade: "))
+resultado_voto = elegibilidade_votacao(idade)
+print(f"O aluno {resultado_voto}.")
 
-idade = ("Informe a sua idade")
+# MISSAO 3
 
-if idade >= 16:
-    print("Você pode votar")
-else:
-    print("Você não pode votar")
+def classificacao (nota_prova):
+    if 90 <= nota_prova <= 100 :
+        return "Parabéns, você tirou A!"
+    elif 80 <= nota_prova < 89 :
+         return "Muito bem, você tirou B."
+    elif 70 <= nota_prova < 79 :
+         return "Bom trabalho, você tirou C."
+    elif 60 <= nota_prova < 69 :
+         return "Fique atento, você tirou D."
+    else:
+         return "Estude um pouco mais, você tirou F."
+    
+nota_prova = float(input("Insira a sua nota: "))
+resultado_prova = classificacao(nota_prova)
+print(f"{resultado_prova}")
+"""
 
-
-# MISSÃO 
-
+# MISSAO 4 
 
