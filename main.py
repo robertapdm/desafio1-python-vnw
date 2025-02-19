1dc561f6eda5238bd373ab94d2241616bd62242e
robertapdm <robertafarmaco@gmail.com>
2025-02-19 13:28:49 -0300

    solving mission 4, 5 and 6

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ print(f"O aluno {resultado_voto}.")
 def classificacao (nota_prova):
     if 90 <= nota_prova <= 100 :
         return "Parabéns, você tirou A!"
-    elif 80 <= nota_prova < 89 :
+    elif 80 <= nota_prova < 90 :
          return "Muito bem, você tirou B."
-    elif 70 <= nota_prova < 79 :
+    elif 70 <= nota_prova < 80 :
          return "Bom trabalho, você tirou C."
-    elif 60 <= nota_prova < 69 :
+    elif 60 <= nota_prova < 70 :
          return "Fique atento, você tirou D."
     else:
          return "Estude um pouco mais, você tirou F."
@@ -39,7 +39,36 @@ def classificacao (nota_prova):
 nota_prova = float(input("Insira a sua nota: "))
 resultado_prova = classificacao(nota_prova)
 print(f"{resultado_prova}")
-"""
 
 # MISSAO 4 
+
+A = int(input("Insira um número inteiro:"))
+B = int(input("Insira outro número inteiro:"))
+
+soma = A+B
+print(f"A soma é", soma)
+
+
+# MISSAO 5
+
+def verificar_senha (senha) :
+    if senha == "Python123" :
+        return "Acesso concedido"
+    else:
+        return "Acesso negado"
+    
+senha = input("Insira a senha:")
+print(f"{verificar_senha(senha)}")
+
+
+# MISSAO 6
+
+contador = 1
+while contador <= 10:
+    print(contador)
+    contador += 1
+
+"""
+
+# MISSAO 7
 
